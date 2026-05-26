ca51478e8995443d98d4cd6f73387100e272ec0a
Ethan Dornian <edornian71@gmail.com>
2026-05-25 21:16:35 -0600

    Client constructor for  connection to server.

--- src/main/java/org/square/Client.java
+++ src/main/java/org/square/Client.java
@@ -1,4 +1,38 @@
 package org.square;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Client {
+
+    //Connection variable
+    private static Connection con;
+
+    public Client()
+    {
+        String url = "jdbc:mysql://localhost:3306/sqre";
+        String user ="root";
+        String password ="123";
+    //try for client connection to the server
+    try
+    {
+        con = DriverManager.getConnection(url,user,password);
+        System.out.println("Connected to the database");
+    }
+    // if connection failed, give out error
+    catch (SQLException e)
+    {
+        System.out.println("Connection Failed!" + e );
+    }
+}
+
+//gets the private connection
+public static Connection getConnection()
+{
+        return con;
+}
+
+
 }
