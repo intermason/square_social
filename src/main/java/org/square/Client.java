fd17c1f0c1f946696d1df70506a60ac9e1982dfa
Ethan Dornian <edornian71@gmail.com>
2026-05-25 22:08:07 -0600

    added insertClient method to input new Clients

--- src/main/java/org/square/Client.java
+++ src/main/java/org/square/Client.java
@@ -25,14 +25,12 @@ public class Client {
     }
 }
 
-
-
     //get information of Clients from Server
     public void getAllClients() {
         // make a query and get results from query
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Client");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM sqre.users");
 
             //prints next client in the database
             while (rs.next())
@@ -46,6 +44,27 @@ public class Client {
             System.out.println("Query Failed!" + e.getMessage());
         }
 
+    }
+
+    // insert new user to the table method, **call in main to insert new client**
+    public void insertClient(String displayName ,String age ,String email,String firstName , String lastName)
+    {
+        String sql = "INSERT INTO users (firstName,LastName,email,Age,DisplayName) VALUES (?,?,?,?,?)";
+
+        //inserts new details for client for the server
+        try (PreparedStatement ps = con.prepareStatement(sql))
+        {
+        ps.setString(1,firstName);
+        ps.setString(2,lastName);
+        ps.setString(3,email);
+        ps.setString(4,age);
+        ps.setString(5,displayName);
+        }
+        //catch errors with query
+        catch (SQLException e)
+        {
+            System.out.println("Insert Failed!" + e.getMessage());
+        }
     }
 
 
